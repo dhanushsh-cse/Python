c136ffb8eba73bf288448464cec58681a363b974
Dhanush S H <dhanushsh700@gmail.com>
2026-08-01 13:55:54 +0530

    Remove Duplicate Python files

--- Area of Circle - Copy.py
+++
@@ -1,4 +0,0 @@
-Radius = float(input("Enter the Radius: "))
-Pi = 3.14
-Area = Pi*Radius*Radius
-print("Area of Circle: ",Area)
--- Sum of Two Numbers - Copy.py
+++
@@ -1,4 +0,0 @@
-a = float(input("Enter the value of a: "))
-b = float(input("Enter the value of b: "))
-Sum = a + b
-print("Addition: ",Sum)
