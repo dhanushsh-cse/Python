c89b7dbefb9e7837308f733545c86c2ad6f340af
Dhanush S H <dhanushsh700@gmail.com>
2026-08-01 16:27:40 +0530

    Create Simple Interest.py

---
+++ Simple Interest.py
@@ -0,0 +1,5 @@
+p = int(input("Enter the Principal(P): "))
+t = float(input("Enter the Time Period(T): "))
+r = float(input("Enter the Rate of Interest(R): "))
+SI = (p*t*r)/100
+print("Simple Interest(SI): ", SI)
