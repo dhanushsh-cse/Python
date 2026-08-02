74d78643f0da1dad31c9a3b0fd590e630e2f2fe8
Dhanush S H <dhanushsh700@gmail.com>
2026-08-02 23:22:00 +0530

    Organize Python programs into topic folders

---
+++ Basics/Swap of Two Numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print("Value of a and b before swaping: \n",a ,b)
+temp = a
+a = b
+b = temp
+print("After swaping")
+print("Value a will be: ", a)
+print("Value b will be: ",b)
