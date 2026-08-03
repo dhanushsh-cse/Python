b0f3365063699b6ebc8bbff9bc5a8e6b9bd0f362
Dhanush S H <dhanushsh700@gmail.com>
2026-08-03 17:57:30 +0530

    Add logical Operator

---
+++ 2.Operators/Logical Operator.py
@@ -0,0 +1,22 @@
+
+a = 10
+b = 20
+
+print("Values:")
+print("a =", a)
+print("b =", b)
+
+# AND Operator
+print("\nLogical AND")
+print("(a < b) and (a > 5) =", (a < b) and (a > 5))
+print("(a > b) and (a > 5) =", (a > b) and (a > 5))
+
+# OR Operator
+print("\nLogical OR")
+print("(a < b) or (a > 20) =", (a < b) or (a > 20))
+print("(a > b) or (a > 20) =", (a > b) or (a > 20))
+
+# NOT Operator
+print("\nLogical NOT")
+print("not(a < b) =", not(a < b))
+print("not(a > b) =", not(a > b))
