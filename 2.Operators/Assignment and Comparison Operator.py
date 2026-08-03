eab7a51d1da6e8659ed9924ecd5807584ee72846
Dhanush S H <dhanushsh700@gmail.com>
2026-08-03 13:13:53 +0530

    Add Assignment and Comparison Operator

---
+++ 2.Operators/Assignment and Comparison Operator.py
@@ -0,0 +1,41 @@
+# Assignment Operators
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("Initial Values:")
+print("a =", a)
+print("b =", b)
+
+a += b      
+print("\na += b :", a)
+
+a -= b    
+print("a -= b :", a)
+
+a *= b      
+print("a *= b :", a)
+
+a /= b      
+print("a /= b :", a)
+
+a //= b     
+print("a //= b :", a)
+
+a %= b      
+print("a %= b :", a)
+
+a = 10
+a **= 2    
+print("a **= 2 :", a)
+
+# Comparison Operators
+x = 10
+y = 20
+
+print("\nComparison Operators:")
+print("x == y :", x == y)
+print("x != y :", x != y)
+print("x > y  :", x > y)
+print("x < y  :", x < y)
+print("x >= y :", x >= y)
+print("x <= y :", x <= y)
