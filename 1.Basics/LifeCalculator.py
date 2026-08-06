1280422920283db46de11d5c7129a57772f70c51
Dhanush S H <dhanushsh700@gmail.com>
2026-08-06 16:05:51 +0530

    Add Life Calculator

---
+++ 1.Basics/LifeCalculator.py
@@ -0,0 +1,9 @@
+age = int(input("Enter the age: "))
+year = int(input("Enter the Number of years: "))
+years_left = year - age
+days_left = years_left*365
+weeks_left = years_left*52
+months_left = years_left*12
+print("Days Left: ",days_left)
+print("weeks left: ", weeks_left)
+print("months left: ", months_left)
