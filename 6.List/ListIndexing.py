e128c1cce7f31df3c3cfc90849b1234e377c3b4f
Dhanush S H <dhanushsh700@gmail.com>
2026-08-08 17:11:54 +0530

    Add List Indexing

---
+++ 6.List/ListIndexing.py
@@ -0,0 +1,13 @@
+num = [ 18,19,21,24,20,26]
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[4])
+print(num[5])
+print(num[-1])
+print(num[-2])
+print(num[-3])
+print(num[-4])
+print(num[-5])
+print(num[-6])
